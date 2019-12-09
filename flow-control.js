f5e5584c29887f336da6cd9e48e52482a90b1e4f
Wendy Raven McNair <wen.mcnair@gmail.com>
2019-12-09 00:58:08 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -2,7 +2,7 @@ function basicTeenager(age) {
   if(age < 13 || age > 19) {
     return "You are not a teenager!";
   } else {
-    return "You are a teenager!";
+    console.log("You are a teenager!");
   }
 }
 
